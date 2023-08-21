a4f5b6ade2b11d1a322f06e7aa67a8ebf87d7762
eduzgun <emreduz00@gmail.com>
2023-08-21 11:38:46 +0100

    added hover effect svg

--- client/assets/history.js
+++ client/assets/history.js
@@ -4,3 +4,32 @@ let paths = Array.from(pathGroup.querySelectorAll("path"));
 
 
 
+function bringToFront(path) {
+    pathGroup.removeChild(path);
+    pathGroup.appendChild(path);
+}
+
+function restoreOrder(path) {
+    // Check if the path is still a child of pathGroup before reinserting
+    if (!pathGroup.contains(path)) {
+        pathGroup.appendChild(path);
+    }
+}
+
+
+paths.forEach(function (item) {
+    var pathLength = item.getTotalLength();
+    item.setAttribute("stroke-dasharray", pathLength);
+    item.setAttribute("stroke-dashoffset", pathLength);
+    item.classList.toggle("new");
+
+    item.addEventListener("mouseover", function () {
+        bringToFront(item);
+    });
+
+    item.addEventListener("mouseout", function () {
+        restoreOrder(item);
+    });
+});
+
+
