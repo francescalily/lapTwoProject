5ad43561e62c39346f777aab9b019ec334f067dc
Franki Callard <francescalily@outlook.com>
2023-08-23 22:35:21 +0100

    getting the separate sections to workgit add .git add .git add . WOOOOO

--- client/assets/js/discussion.js
+++ client/assets/js/discussion.js
@@ -14,16 +14,18 @@ const allPosts = [];
 
 let isOpen = false;
 
-libraryBtn.addEventListener("click", open);
-recyclingBtn.addEventListener("click", open);
-craftsBtn.addEventListener("click", open);
-historyBtn.addEventListener("click", open);
+libraryBtn.addEventListener("click", openLibrary);
+recyclingBtn.addEventListener("click", openRecycling);
+craftsBtn.addEventListener("click", openCrafts);
+historyBtn.addEventListener("click", openHistory);
 
 function createPostElement(data) {
   const post = document.createElement("div");
   post.className = "post";
 
-  const username = document.createElement("h3");
+  post.setAttribute("data-topic", data["topic"]);
+
+  const username = document.createElement("h2");
   username.textContent = data["username"];
   post.appendChild(username);
 
@@ -38,7 +40,7 @@ function createPostElement(data) {
   return post;
 }
 
-function open() {
+function openHistory() {
   const divBox = document.createElement("div");
   divBox.style.width = "90%";
   divBox.style.height = "700px";
@@ -51,15 +53,11 @@ function open() {
   divBox.style.boxShadow = "0px 0px 10px rgba(0,0,0,0.5)";
   divBox.style.overflow = "scroll";
 
-  // const dataBox = document.createElement("div");
-  // dataBox
-
   allPosts.forEach((postElem) => {
-    divBox.appendChild(postElem.cloneNode(true));
+    if (postElem.getAttribute("data-topic") === "history") {
+      divBox.appendChild(postElem.cloneNode(true));
+    }
   });
-
-  const helloText = document.createTextNode("Hello");
-  divBox.appendChild(helloText);
 
   const exitButton = document.createElement("button");
   exitButton.textContent = "Exit";
@@ -67,17 +65,118 @@ function open() {
   exitButton.style.position = "absolute";
   exitButton.style.top = "8px";
   exitButton.style.right = "16px";
-  // exitButton.style.top = "80%";
-  // exitButton.style.left = "50%";
 
   exitButton.addEventListener("click", function () {
     document.body.removeChild(divBox);
   });
 
-  // Append the exit button to divBox
   divBox.appendChild(exitButton);
 
-  // Append divBox to the body of the document
+  document.body.appendChild(divBox);
+}
+
+function openRecycling() {
+  const divBox = document.createElement("div");
+  divBox.style.width = "90%";
+  divBox.style.height = "700px";
+  divBox.style.backgroundColor = "lightgray";
+  divBox.style.position = "fixed";
+  divBox.style.top = "50%";
+  divBox.style.left = "50%";
+  divBox.style.transform = "translate(-50%, -50%)";
+  divBox.style.padding = "20px";
+  divBox.style.boxShadow = "0px 0px 10px rgba(0,0,0,0.5)";
+  divBox.style.overflow = "scroll";
+
+  allPosts.forEach((postElem) => {
+    if (postElem.getAttribute("data-topic") === "recycling") {
+      divBox.appendChild(postElem.cloneNode(true));
+    }
+  });
+
+  const exitButton = document.createElement("button");
+  exitButton.textContent = "Exit";
+  exitButton.style.marginTop = "20px";
+  exitButton.style.position = "absolute";
+  exitButton.style.top = "8px";
+  exitButton.style.right = "16px";
+
+  exitButton.addEventListener("click", function () {
+    document.body.removeChild(divBox);
+  });
+
+  divBox.appendChild(exitButton);
+
+  document.body.appendChild(divBox);
+}
+
+function openCrafts() {
+  const divBox = document.createElement("div");
+  divBox.style.width = "90%";
+  divBox.style.height = "700px";
+  divBox.style.backgroundColor = "lightgray";
+  divBox.style.position = "fixed";
+  divBox.style.top = "50%";
+  divBox.style.left = "50%";
+  divBox.style.transform = "translate(-50%, -50%)";
+  divBox.style.padding = "20px";
+  divBox.style.boxShadow = "0px 0px 10px rgba(0,0,0,0.5)";
+  divBox.style.overflow = "scroll";
+
+  allPosts.forEach((postElem) => {
+    if (postElem.getAttribute("data-topic") === "crafts") {
+      divBox.appendChild(postElem.cloneNode(true));
+    }
+  });
+
+  const exitButton = document.createElement("button");
+  exitButton.textContent = "Exit";
+  exitButton.style.marginTop = "20px";
+  exitButton.style.position = "absolute";
+  exitButton.style.top = "8px";
+  exitButton.style.right = "16px";
+
+  exitButton.addEventListener("click", function () {
+    document.body.removeChild(divBox);
+  });
+
+  divBox.appendChild(exitButton);
+
+  document.body.appendChild(divBox);
+}
+
+function openLibrary() {
+  const divBox = document.createElement("div");
+  divBox.style.width = "90%";
+  divBox.style.height = "700px";
+  divBox.style.backgroundColor = "lightgray";
+  divBox.style.position = "fixed";
+  divBox.style.top = "50%";
+  divBox.style.left = "50%";
+  divBox.style.transform = "translate(-50%, -50%)";
+  divBox.style.padding = "20px";
+  divBox.style.boxShadow = "0px 0px 10px rgba(0,0,0,0.5)";
+  divBox.style.overflow = "scroll";
+
+  allPosts.forEach((postElem) => {
+    if (postElem.getAttribute("data-topic") === "library") {
+      divBox.appendChild(postElem.cloneNode(true));
+    }
+  });
+
+  const exitButton = document.createElement("button");
+  exitButton.textContent = "Exit";
+  exitButton.style.marginTop = "20px";
+  exitButton.style.position = "absolute";
+  exitButton.style.top = "8px";
+  exitButton.style.right = "16px";
+
+  exitButton.addEventListener("click", function () {
+    document.body.removeChild(divBox);
+  });
+
+  divBox.appendChild(exitButton);
+
   document.body.appendChild(divBox);
 }
 
@@ -109,7 +208,6 @@ document.getElementById("post-form").addEventListener("submit", async (e) => {
   console.log(responseData);
 
   if ((result.status = 201)) {
-    // let container = document.getElementById("posts");
     const newPostElem = createPostElement(responseData);
     allPosts.push(newPostElem);
 
@@ -131,26 +229,12 @@ async function loadPosts() {
 
   if (response.status == 200) {
     const posts = await response.json();
+    const container = document.getElementById("posts");
 
     posts.forEach((p) => {
-      let container;
-      switch (p.topic) {
-        case "library":
-          container = document.querySelector(".library-posts");
-          break;
-        case "recycling":
-          container = document.querySelector(".recycling-posts");
-          break;
-        case "knowledge":
-          container = document.querySelector(".knowledge-posts");
-        case "history":
-          container = document.querySelector(".history-posts");
-
-        default:
-          container = document.getElementById("posts");
-      }
       const elem = createPostElement(p);
-      allPosts.push(elem); // Add each post to the global array
+
+      allPosts.push(elem);
       container.appendChild(elem);
     });
   } else {
@@ -160,6 +244,21 @@ async function loadPosts() {
 
 loadPosts();
 
+//let container;
+// switch (p.topic) {
+//   case "library":
+//     container = document.querySelector(".library-posts");
+//     break;
+//   case "recycling":
+//     container = document.querySelector(".recycling-posts");
+//     break;
+//   case "knowledge":
+//     container = document.querySelector(".knowledge-posts");
+//   case "history":
+//     container = document.querySelector(".history-posts");
+
+//   default:
+//     container = document.
 //need to add username - join sql table with valentin user_account table so that username appears with post
 //need to display posts (front-end)- right now can see the posts as soon as pressing post - need to make a toggle so is hidden until button is pressed
 //(backend) need to make sure that data is stored correctly and is secure
