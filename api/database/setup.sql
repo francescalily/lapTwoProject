02783588ac98e55be3d4433e0be5a0d0ad972731
eduzgun <emreduz00@gmail.com>
2023-08-24 20:33:31 +0100

    removed insertions from setup.sql

--- api/database/setup.sql
+++ api/database/setup.sql
@@ -28,28 +28,16 @@ CREATE TABLE user_account (
     PRIMARY KEY (user_id)
 );
 
-INSERT INTO user_account
-    (username, isadmin, password)
-VALUES
-('Harry', TRUE, FALSE),
-('Helly', TRUE, FALSE);
-
-
 
 CREATE TABLE community (
     user_id INT GENERATED ALWAYS AS IDENTITY,
-    -- username VARCHAR(30),
+    username VARCHAR(30) NOT NULL,
     topic VARCHAR(50) NOT NULL,
     post VARCHAR(300) NOT NULL,
     votes INT DEFAULT 0,
     FOREIGN KEY (user_id) REFERENCES user_account (user_id)
 );
 
-INSERT INTO community
-    (topic, post, votes)
-VALUES
-('History', 'I love how the git commited team are working on the history of Florin!', 0),
-('Library', 'I think this website is the best website I have ever been on:---) I love being able to get good book reccomendations.', 0);
 
 
 
