d665d1dc70e60a25b56b49125efa384141cf6448
eduzgun <emreduz00@gmail.com>
2023-08-24 11:55:37 +0100

    test for wrong login details

--- api/tests/user.spec.js
+++ api/tests/user.spec.js
@@ -1,25 +1,37 @@
 const request = require('supertest');
-const app = require("../api")
+const app = require("../api");
 
 describe('User Controller', () => {
     it('should register a new user', async () => {
         const response = await request(app)
         .post('/users/register')
         .send({
-            username: 'tester',
-            email: 'test@testing.com',
+            username: 'tester5',
+            email: 'test5@testing.com',
             password: 'test123'
         });
     expect(response.status).toBe(201);
     })
 
-    it('should login with existing user credentials', async () => {
+    it('Can login with existing user login credentials', async () => {
         const response = await request(app)
         .post('/users/login')
         .send({
-            email: 'test@testing.com',
+            email: 'test5@testing.com',
             password: 'test123'
         });
+        console.log(response);
     expect(response.status).toBe(200);
     })
+
+    it ('Prompt user when wrong login credentials submitted', async () => {
+        const response = await request(app)
+        .post('/user/login')
+        .send({
+            email: "fake@imail.com",
+            password: "wrong"
+        })
+    expect(response.text).toBe("Wrong login details");
+    })
+
 });
