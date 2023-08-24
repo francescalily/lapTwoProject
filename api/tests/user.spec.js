a2e9199db2d2ebe6c4b4189d576d968801d4625a
eduzgun <emreduz00@gmail.com>
2023-08-24 14:42:00 +0100

    generate fake email and username for tests

--- api/tests/user.spec.js
+++ api/tests/user.spec.js
@@ -1,17 +1,24 @@
 const request = require('supertest');
 const app = require("../api");
+const { faker } = require('@faker-js/faker');
+
 
 describe('User Controller', () => {
-    it('should register a new user', async () => {
+    it('Register a new user with new credentials', async () => {
+        // Generate random email and username
+        const uniqueEmail = faker.internet.email();
+        const uniqueUsername = faker.internet.userName();
+        const uniquePassword = 'testPassword'; 
+
         const response = await request(app)
-        .post('/users/register')
-        .send({
-            username: 'tester5',
-            email: 'test5@testing.com',
-            password: 'test123'
-        });
-    expect(response.status).toBe(201);
-    })
+            .post('/users/register')
+            .send({
+                username: uniqueUsername,
+                email: uniqueEmail,
+                password: uniquePassword
+            });
+        expect(response.status).toBe(201);
+    });
 
     it('Can login with existing user login credentials', async () => {
         const response = await request(app)
@@ -20,18 +27,19 @@ describe('User Controller', () => {
             email: 'test5@testing.com',
             password: 'test123'
         });
-        console.log(response);
     expect(response.status).toBe(200);
     })
 
     it ('Prompt user when wrong login credentials submitted', async () => {
+        try { 
         const response = await request(app)
         .post('/user/login')
         .send({
             email: "fake@imail.com",
             password: "wrong"
         })
-    expect(response.text).toBe("Wrong login details");
+        } catch (err) {
+            expect(err.message).toBe('duplicate key value violates unique constraint "user_account_username_key"')
+        }
     })
-
 });
